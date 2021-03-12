8bb1de80f2d8c7a6ca9b2eea20bd54ecb72b1a3a
lorisboyer <loris.boyer@gmail.com>
2021-03-12 15:57:46 +0100

    CaPushSevere

---
+++ AppMobile/ProjetGroupe/ProjetGroupe/AppShell.xaml.cs
@@ -0,0 +1,23 @@
+﻿using ProjetGroupe.ViewModels;
+using ProjetGroupe.Views;
+using System;
+using System.Collections.Generic;
+using Xamarin.Forms;
+
+namespace ProjetGroupe
+{
+    public partial class AppShell : Xamarin.Forms.Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+            Routing.RegisterRoute(nameof(ItemDetailPage), typeof(ItemDetailPage));
+            Routing.RegisterRoute(nameof(NewItemPage), typeof(NewItemPage));
+        }
+
+        private async void OnMenuItemClicked(object sender, EventArgs e)
+        {
+            await Shell.Current.GoToAsync("//LoginPage");
+        }
+    }
+}
---
+++ AppMobile/ProjetGroupe/ProjetGroupe/Views/LoginPage.xaml.cs
@@ -0,0 +1,21 @@
+﻿using ProjetGroupe.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace ProjetGroupe.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class LoginPage : ContentPage
+    {
+        public LoginPage()
+        {
+            InitializeComponent();
+            this.BindingContext = new LoginViewModel();
+        }
+    }
+}
