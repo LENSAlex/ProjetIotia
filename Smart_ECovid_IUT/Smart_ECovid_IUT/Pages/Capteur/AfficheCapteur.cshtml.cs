1b3556fae7a78da3230c62bebde9ad342fe0a4b6
pennaccchioni dorian <pennacchioni.dorian@gmail.com>
2021-03-25 16:40:54 +0100

    Tableau de bord : Card . Dossier Class et page web Fichier page web

---
+++ Smart_ECovid_IUT/Smart_ECovid_IUT/Pages/Capteur/AfficheCapteur.cshtml.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Smart_ECovid_IUT.Pages.Capteur
+{
+    public class AfficheCapteurModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
