47d2d7da2ddb9f892f58a1b1db81b7dae09d15d7
pennaccchioni dorian <pennacchioni.dorian@gmail.com>
2021-03-26 08:55:21 +0100

    Utilisateur

---
+++ Smart_ECovid_IUT/ClasseE-Covid/IOTDevise/IOTDevise.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClasseE_Covid.IOTDevise
+{
+    class IOTDevise
+    {
+    }
+}
---
+++ Smart_ECovid_IUT/ClasseE-Covid/Utilisateur/Utilisateur.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClasseE_Covid.Utilisateur
+{
+    class Utilisateur
+    {
+    }
+}
