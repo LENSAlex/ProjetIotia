2446db4664caabbd4d0a15124f94a72a95e4c9f4
pennaccchioni dorian <pennacchioni.dorian@gmail.com>
2021-04-01 11:52:04 +0200

    Debut API

---
+++ Smart_ECovid_IUT/ClasseE-Covid/GitHubBranch.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using Newtonsoft.Json;
+using System.Text.Json;
+namespace ClasseE_Covid
+{
+    public class GitHubBranch
+    {
+        [JsonProperty("name")]
+        public string Name { get; set; }
+
+       //[JsonProperty("commit")]
+       //public string Commit { get; set; }
+
+        [JsonProperty("protected")]
+        public string Protected { get; set; }
+
+    } 
+}
--- Smart_ECovid_IUT/Smart_ECovid_IUT/Pages/Privacy.cshtml.cs
+++ Smart_ECovid_IUT/Smart_ECovid_IUT/Pages/Privacy.cshtml.cs
@@ -4,21 +4,56 @@ using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Threading.Tasks;
+using ClasseE_Covid;
+using System.Net;
+using System.Text.Json;
+//using Newtonsoft.Json;
+
 
 namespace Smart_ECovid_IUT.Pages
 {
     public class PrivacyModel : PageModel
     {
+        public string element { get; set; }
         private readonly ILogger<PrivacyModel> _logger;
 
-        public PrivacyModel(ILogger<PrivacyModel> logger)
+        private readonly IHttpClientFactory _clientFactory;
+
+         public IEnumerable<GitHubBranch> Branches { get; private set; } //GitHubBranch et une class
+
+        public bool GetBranchesError { get; private set; }
+
+        public PrivacyModel(ILogger<PrivacyModel> logger, IHttpClientFactory clientFactory)
         {
             _logger = logger;
+            _clientFactory = clientFactory;
         }
 
-        public void OnGet()
+        public async Task OnGet() // tache asycrene attent une reponce tache awit 
         {
+            var request = new HttpRequestMessage(HttpMethod.Get,
+            "https://api.github.com/repos/dotnet/AspNetCore.Docs/branches");
+            request.Headers.Add("Accept", "application/vnd.github.v3+json");
+            request.Headers.Add("User-Agent", "HttpClientFactory-Sample");
+
+            var client = _clientFactory.CreateClient();
+
+            var response = await client.SendAsync(request); // vus que la fonction est async elle vas s'arreter ici pour attendre une reponce 
+
+            if (response.IsSuccessStatusCode)
+            {
+                using var responseStream = await response.Content.ReadAsStreamAsync();
+                Branches = await JsonSerializer.DeserializeAsync
+                <IEnumerable<GitHubBranch>>(responseStream);
+            }
+            else
+            {
+                GetBranchesError = true;
+                Branches = Array.Empty<GitHubBranch>();
+            }
         }
     }
 }
+
