9f6055d9bc1cf61b3659c6bc5da2fb9fa2b47e1f
LENSAlex <alex.lens2000@gmail.com>
2021-04-01 10:44:09 +0200

    Bdd IUT

--- WebService/ServicePersonne.js
+++ WebService/ServicePersonne.js
@@ -7,10 +7,10 @@ const port = 3000;
 // BDD--------------------
 console.log('Get connection ...');
 var conn = mysql.createConnection({
-  database: 'test',
-  host: "51.75.125.121",
-  user: "alex",
-  password: "*"
+    database: 'test', //Voir database
+    host: "51.75.125.121", //134.59.143.55:8081
+    user: "alex", //ioita
+    password: "*" //Sm@rt$2021
 });
 
 //https://www.sitepoint.com/using-node-mysql-javascript-client/
@@ -79,31 +79,28 @@ var conn = mysql.createConnection({
 
 
 //res.status(200).json({ Msg: 'test' });
-app.get("/Personne" , (req,res) =>
-{
+app.get("/Personne", (req, res) => {
     //Voir la table 
     //Requete possible: Select nom , prenom , Formation_nom from Personne inner join Contenir
 
     //liste de tous les eleves des promos
     conn.connect(function(err) {
-    if (err)
-        res.status(404).json({ ErrorConnection: 'Error bdd' });
-    console.log("Connected!");
-        conn.query("SELECT * from Authentification", function (err, result) {
         if (err)
-            res.status(400).json({ ErrorRequete: 'Requete invalid' });
-        else
-        {
-            res.status(200).json(result);
-            console.log(result);
-        }
+            res.status(404).json({ ErrorConnection: 'Error bdd' });
+        console.log("Connected!");
+        conn.query("SELECT * from Authentification", function(err, result) {
+            if (err)
+                res.status(400).json({ ErrorRequete: 'Requete invalid' });
+            else {
+                res.status(200).json(result);
+                console.log(result);
+            }
         });
     });
 })
 
 
-app.get("/Personne/Formation" , (req,res) =>
-{
+app.get("/Personne/Formation", (req, res) => {
     //Requete possible: Select Formation_nom from Formation;
 
     //liste des promos
@@ -111,47 +108,41 @@ app.get("/Personne/Formation" , (req,res) =>
 })
 
 
-app.get("/Personne/Departement/:ID" , (req,res) =>
-{
+app.get("/Personne/Departement/:ID", (req, res) => {
     //Requete possible:
 
     //liste des personnes pour un departement
     //Voir avec pierre car bcp inner join peut etre pas bon
 })
 
-app.get("/Personne/Promotion/:ID" , (req,res) =>
-{
+app.get("/Personne/Promotion/:ID", (req, res) => {
     //Requete possible:select P.nom , P.prenom from Promotion P inner join Contenir C C.id_formation=P.id_Formation 
     //inner join Personne P on P.id_Personne=C.id_Eleve 
 
     //liste des personnes dans une promo
 })
 
-app.get("/Personne/Promotion/:IDPromo/personne/cours" , (req,res) =>
-{
+app.get("/Personne/Promotion/:IDPromo/personne/cours", (req, res) => {
     //Requete possible:select heure_debut , heure_fin ,Commentaire from Cours where id_promotion = '"+req.params.IDPromo+"'
 
     //Liste des cours d un eleve
 })
 
-app.get("/Personne/Promotion/:IDPromo/personne/cours/presentielle" , (req,res) =>
-{
+app.get("/Personne/Promotion/:IDPromo/personne/cours/presentielle", (req, res) => {
     //Requete possible:select heure_debut , heure_fin ,Commentaire from Cours where id_promotion = '"+req.params.IDPromo+"' and presentiel = true
 
     //Liste des cours en presentielle d un eleve
 })
 
 
-app.get("/Personne/Promotion/:IDPromo/personne/cours/distancielle" , (req,res) =>
-{
+app.get("/Personne/Promotion/:IDPromo/personne/cours/distancielle", (req, res) => {
     //Requete possible:select heure_debut , heure_fin ,Commentaire from Cours where id_promotion = '"+req.params.IDPromo+"' and presentiel = false
 
     //Liste des cours en distancielle d un eleve
 })
 
 
-app.get("/Personne/:IDPersonne/abs" , (req,res) =>
-{
+app.get("/Personne/:IDPersonne/abs", (req, res) => {
     //Requete possible:select C.Commentaire , A.motif from Cours C inner join Absence A on A.id_cours = C.id_cours where A.id_PersonneEtudiant = '"+req.params.IDPersonne+"'
 
     //Absecence d un eleve
@@ -161,14 +152,12 @@ app.get("/Personne/:IDPersonne/abs" , (req,res) =>
 
 //Route annexes qui doivent etre utiliser avant de passer des parametre route (idUser , IdPromo , ...)
 //Id personne voir https
-app.get("/Personne/GetIDPerson/:login/:mdp" , (req,res) =>
-{
+app.get("/Personne/GetIDPerson/:login/:mdp", (req, res) => {
     //
 })
 
 //IDPromo a savoir car je n ai pas de nom pour identifier
 
-app.listen(port , () =>
-{
+app.listen(port, () => {
     console.log("Serveur disponible port " + port);
 })
