a2f8031aa796efac26497f816c102a5f7fc1560a
vir zal <dragnen404@gmail.com>
2021-04-09 09:55:44 +0200

    t

--- Smart_ECovid_IUT/Smart_ECovid_IUT/Pages/Promotion/CréeModifPromotion.cshtml.cs
+++ Smart_ECovid_IUT/Smart_ECovid_IUT/Pages/Promotion/CréeModifPromotion.cshtml.cs
@@ -1,7 +1,11 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Text.Json;
 using System.Threading.Tasks;
+using ClasseE_Covid;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -9,8 +13,82 @@ namespace Smart_ECovid_IUT.Pages.Promotion
 {
     public class CréeModifPromotionModel : PageModel
     {
-        public void OnGet()
+        private readonly IHttpClientFactory _clientFactory;
+
+        public IEnumerable<GitHubBranch> Branches { get; private set; } //GitHubBranch et une class
+
+        public bool GetBranchesError { get; private set; }
+
+        public CréeModifPromotionModel(IHttpClientFactory clientFactory)
         {
+            _clientFactory = clientFactory;
+        }
+        public async Task OnGet()
+        {
+
+        }
+        internal static async Task<string> Save(GitHubBranch item)
+        {
+            var httpClient = new HttpClient();
+            string WebAPIUrl = "http://51.77.137.170:8080/equipes";
+            Uri uri = new Uri(WebAPIUrl);
+            httpClient.DefaultRequestHeaders.Add("User-Agent", ".NET Foundation Repository Reporter");
+            StringBuilder sb = new StringBuilder();
+            if (item != null)
+            {
+                sb.Append(@"{""Id"" : " + item.Id + ",");
+                sb.Append(@"""name"" : """ + item.Name + @""",");
+                sb.Append(@"""disponible"" : " + item.Disponible );
+                sb.Append(@"""}");
+
+                string jsonData = sb.ToString();
+                //string json = JsonConvert.SerializeObject(item); A utiliser que si envoie d'une liste complète dès le départ.
+                StringContent content = new StringContent(jsonData, Encoding.UTF8, "application/json");
+                try
+                {
+                    var response = await httpClient.PostAsync(uri, content);
+                    if (response.IsSuccessStatusCode)
+                    {
+                        return "Ok";
+                    }
+                    else
+                    {
+                        return "ErreurStatusCode";
+                    }
+                }
+                catch (Exception e)
+                {
+                   // Debug.WriteLine(e.Message);
+                    return "ErreurTryCatch";
+                }
+            }
+            else
+            {
+               // Debug.WriteLine("Erreur dans Save Capteur");
+                return "ErreurCapteurNull";
+            }
+        }
+
+        public void OnPostEnvoieDonne()
+        {
+            GitHubBranch gitHubBranch = new GitHubBranch();
+
+            gitHubBranch.Id = Convert.ToInt32(Request.Form["id"]);
+            gitHubBranch.Name = Request.Form["nom"];
+
+            if (Request.Form["duree"] == "1")
+                gitHubBranch.Disponible = true;
+            else
+                gitHubBranch.Disponible = false;
+
+            GetDataAsync(gitHubBranch);
+        }
+        public async Task GetDataAsync(GitHubBranch gitHubBranch)
+        {
+            await Save(gitHubBranch);
+
+            Response.Redirect("/Promotion/ListePromotion");
         }
     }
 }
+
