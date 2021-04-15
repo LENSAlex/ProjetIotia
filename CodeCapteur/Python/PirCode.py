c5002b6e63aba5aea71d4698177d81a1c5fbc952
Thibaud Charbonnel <thibaud.charbonnel@etu.unice.fr>
2021-04-15 15:06:02 +0200

    maj code python

--- CodeCapteur/Python/PirCode.py
+++ CodeCapteur/Python/PirCode.py
@@ -22,16 +22,15 @@ Mais requete bdd pour savoir personne dans cette salle
 
 from bluetooth import *
 import socket
+import tkinter as tk
 from datetime import datetime
-from time import sleep
+import time
 import re
 import paho.mqtt.client as mqtt
-import signal
-import sys
 
 #date de prise
-broker_address="192.168.143.136"
-client = mqtt.Client("capteur")
+
+
 now=datetime.now()
 date_time=now.strftime("%Y/%d/%m %H:%M:%S")
 dat=str(date_time)
@@ -51,63 +50,60 @@ for name, addr in nearby_devices:
         print("OK_adr_mac\n")
         break
 
-"""
+
 client_socket_haut_parleur=socket.socket(socket.AF_BLUETOOTH,socket.SOCK_STREAM, socket.BTPROTO_RFCOMM) #m5Stack
 client_socket_haut_parleur.connect(("84:0D:8E:3D:3C:56",1))
 """
 client_socket_gaz=socket.socket(socket.AF_BLUETOOTH,socket.SOCK_STREAM, socket.BTPROTO_RFCOMM)
-client_socket_gaz.connect(("50:02:91:8D:10:36",1))
+client_socket_gaz.connect(("50:02:91:8D:10:36",1))# utiliser également pour tester le pir sortie
 client_socket_fenetre=socket.socket(socket.AF_BLUETOOTH,socket.SOCK_STREAM, socket.BTPROTO_RFCOMM)
 client_socket_fenetre.connect(("50:02:91:8D:DC:56",1))
+"""
 client_socket_pir=socket.socket(socket.AF_BLUETOOTH,socket.SOCK_STREAM, socket.BTPROTO_RFCOMM)
 client_socket_pir.connect(("24:A1:60:46:AD:3E",1))
 
-def signal_handler(sig, frame):
-    print('You pressed Ctrl+C!')
-    client_socket_gaz.close()
-    #client_socket_haut_parleur.close()
-    client_socket_fenetre.close()
-    client_socket_pir.close()
-    sys.exit(0)
+client_socket_pir_sortie=socket.socket(socket.AF_BLUETOOTH,socket.SOCK_STREAM, socket.BTPROTO_RFCOMM)
+client_socket_pir_sortie.connect(("50:02:91:8D:DC:56",1))#modifier l'adresse mac en fonction 
 
-signal.signal(signal.SIGINT, signal_handler)
+#mqtt.connect("192.168.143.136")
+#mqtt.loop_start()
 
 while True:
     size = 1024
-    #Reception data pour gaz
+    """
+    #Reception data pour gaz 
     data_gaz = str(client_socket_gaz.recv(size))
     res_gaz = re.sub(r"[^Z0-9]","",data_gaz)
     dataGaz = res_gaz[0:3]
     print(dataGaz)
-    sleep(1)
-    client.connect(broker_address)
-    client.publish("/paho/gaz", dataGaz)
     #Reception data pour fenetre
     data_fenetre = str(client_socket_fenetre.recv(size))
     res_fenetre = re.sub(r"[^Z0-9]","",data_fenetre)
     datafentre = res_fenetre[0:1]
+    """
     #Reception data pour pir
     data_pir = str(client_socket_pir.recv(size))
     res_pir = re.sub(r"[^Z0-9]","",data_pir)
     data_pir = res_pir[0:1]
-    sleep(1)
-    client.connect(broker_address)
-    client.publish("/paho/pir", data_pir)
-    print(datafentre)
-    sleep(1)
-    client.connect(broker_address)
-    client.publish("/paho/fenetre", datafentre)   
+    #Reception data pour pir sortie
+    data_pir_sortie = str(client_socket_pir_sortie.recv(size))
+    res_pir_sortie = re.sub(r"[^Z0-9]","",data_pir_sortie)
+    data_pir_sortie = res_pir_sortie[0:1]
+    #print(datafentre)
+    #mqtt.publish("data/temperature", 5) 
+    
     """
     if datafentre == "0" and dataGaz > "210":
         print("ferme")
-        msg=("GazAlerte").encode('utf-8');
+        msg=("GazAlerte").encode('utf-8'); 
         client_socket_haut_parleur.send(msg);
         time.sleep(5)
-   
+    """
     print("Pir" + data_pir)
+    print("Pir sortie" + data_pir_sortie)
     if data_pir == "1":
         print("dedans pir");
-        msg=("PirEntreeBL").encode('utf-8');
+        msg=("PirEntreeBL").encode('utf-8'); 
         client_socket_haut_parleur.send(msg);
         time.sleep(2)
         #print("dedans");
@@ -117,22 +113,19 @@ while True:
             #data_pir = data_pir[0:1];
         #    print("attente 0")
         #    time.sleep(2)
-       
-    #print(dataGaz)
-    """
+    if data_pir_sortie == "1":
+        print("dedans pir sortie");
+        msg=("PirSortieBL").encode('utf-8'); 
+        client_socket_haut_parleur.send(msg);
+        time.sleep(2)
 
 
+"""
 client_socket_gaz.close()
 """
 client_socket_haut_parleur.close()
 """
 client_socket_fenetre.close()
+"""
 client_socket_pir.close()
-
-        
-    #print(dataGaz)
-client_socket_gaz.close()
-client_socket_haut_parleur.close()
-client_socket_fenetre.close()
 client_socket_pir.close()
-
