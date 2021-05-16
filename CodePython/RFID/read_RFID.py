d839f4c67c213d3c4c668415c7388f51e2e062cc
TheoDP06 <77000523+TheoDP06@users.noreply.github.com>
2021-05-16 16:34:39 +0200

    Update read_RFID.py
    commentaire du programme

--- CodePython/RFID/read_RFID.py
+++ CodePython/RFID/read_RFID.py
@@ -1,28 +1,40 @@
-#!/usr/bin/env python
-# -*- coding: utf8 -*-
-# Version modifiee de la librairie https://github.com/mxgxw/MFRC522-python
+#Librairie python 
 
+#gpio raspberry
 import RPi.GPIO as GPIO
+#librairie lecteur RFID
 import MFRC522
 import signal
+#librairie écran lcd
 import drivers
+#librairie pour client mqtt
 import paho.mqtt.client as mqtt
 
+#timer
 from time import sleep
 
+#addresse ip broker mqtt
 broker_address="192.168.143.136"
+
+#nomde notre client mqtt
 client = mqtt.Client("RFID_IUD")
 
+#initialisation des pin GPIO
 GPIO.setmode(GPIO.BOARD)
+#variable pour le driver lcd
 display = drivers.Lcd()
+#lecture mqtt
 continue_reading = True
 
+#couleur pour la led 
 GREEN = 11
 RED = 13
 
+#setup des pins pour dire qu'ils sont en sortie
 GPIO.setup(GREEN, GPIO.OUT)
 GPIO.setup(RED, GPIO.OUT)
-#Fonction qui arrete la lecture proprement 
+
+#Fonction qui arrete la lecture et le programme proprement 
 def end_read(signal,frame):
     display.lcd_clear()
     global continue_reading
@@ -31,6 +43,8 @@ def end_read(signal,frame):
     GPIO.cleanup()
 
 signal.signal(signal.SIGINT, end_read)
+
+#Lectrure protocole MIFARE pour le lecteur RFID
 MIFAREReader = MFRC522.MFRC522()
 
 print ("Passer le tag RFID a lire")
@@ -38,38 +52,49 @@ display.lcd_display_string(" Passer le tag:",1)
 display.lcd_display_string("      RFID",2)
 
 while continue_reading:
+    #extinction des pins
     GPIO.output(GREEN, GPIO.LOW)
     GPIO.output(RED, GPIO.LOW)
     display.lcd_display_string(" Passer le tag:   ",1)
-    display.lcd_display_string("                ",2)
-    # Detecter les tags
+    display.lcd_display_string("      RFID",2)
+	
+    #Detecte les tags
     (status,TagType) = MIFAREReader.MFRC522_Request(MIFAREReader.PICC_REQIDL)
 
-    # Une carte est detectee
+    #Une carte a été detecté
     if status == MIFAREReader.MI_OK:
         print ("Carte detectee")
     
-    # Recuperation UID
+    #Recuperation UID
     (status,uid) = MIFAREReader.MFRC522_Anticoll()
 
     if status == MIFAREReader.MI_OK:
 	display.lcd_clear()
+	#integration de l'iud dans une variable string
 	UID = str(uid[0])+str(uid[1])+str(uid[2])+str(uid[3])
 	#print (UID)
 	display.lcd_display_string("UID:"+UID,1)
+	#affiche l'IUD
         print ("UID de la carte : "+str(uid[0])+"."+str(uid[1])+"."+str(uid[2])+"."+str(uid[3]))
 	sleep(1)
+	
 	if UID == "21713017272":
-		client.connect(broker_address) #connect to broker
+		#connection au broker
+		client.connect(broker_address)
+		#publie sur le topic
 		client.publish("/batiment/etages/salles/UID",UID)
 		display.lcd_clear()
+		#accès autoriser et allume le led en vert
 		display.lcd_display_string("      Acces",1)
 		display.lcd_display_string("    Autorisee",2)
 		GPIO.output(GREEN, GPIO.HIGH)
 		sleep(1)
 	else:
-		client.connect(broker_address) #connect to broker
+		#connection au broker
+		client.connect(broker_address)
+		#publie sur le topic
                 client.publish("/batiment/etages/salles/UID",UID)
+		#accès autoriser et allume le led en rouge
 		display.lcd_clear()
 		display.lcd_display_string("     Acces",1)
                 display.lcd_display_string("    Refusee",2)
