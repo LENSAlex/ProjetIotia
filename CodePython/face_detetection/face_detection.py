b765509df53863355366a9ce34bfcd0807de37ce
TheoDP06 <77000523+TheoDP06@users.noreply.github.com>
2021-05-16 16:42:52 +0200

    Update face_detection.py

--- CodePython/face_detetection/face_detection.py
+++ CodePython/face_detetection/face_detection.py
@@ -9,7 +9,7 @@ import sys
 #libairie mqtt client
 import paho.mqtt.client as mqtt
 
-#addresse ip broker mqtt
+#adresse ip broker mqtt
 broker_address = "192.168.143.136"
 
 #nom de notre client mqtt
@@ -21,20 +21,20 @@ face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
 #http://192.168.1.114:99/videostream.cgi?user=admin&pwd=&resolution=32&rate=0
 #cap = cv2.VideoCapture("http://192.168.143.141:8081/")
 
-#lecture du flux video de la camera connecter en USB
+#lecture du flux video de la camera connecté en USB
 cap = cv2.VideoCapture(0)
 print("lancement du flux vidéo")
 
 while True:
     _, img = cap.read()
-    #copnvertie l'image en gris 
+    #convertie l'image en gris 
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     #detecte les visages 
     faces = face_cascade.detectMultiScale(gray, 1.1, 4)
     #dessine un rectancgle autour du visages
     for (x, y, w, h) in faces:
         cv2.rectangle(img, (x, y), (x+w, y+h), (255, 0, 0), 2)
-    #viriables qui enregistre le nombres de rectangles qu'il detecte
+    #variables qui enregistre le nombres de rectangles qu'il detecte
     nb_pers = len(faces)
     #print(nb_pers)
     
