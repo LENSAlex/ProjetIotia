1d901180cf36137ae1aa85fc27831d02a8e894e3
TheoDP06 <77000523+TheoDP06@users.noreply.github.com>
2021-05-16 16:40:57 +0200

    Update face_detection.py
    comment programme

--- CodePython/face_detetection/face_detection.py
+++ CodePython/face_detetection/face_detection.py
@@ -1,38 +1,51 @@
+#Librairie
+
+#librairie OpenCV
 import cv2
+#timer
 from time import sleep
+
 import sys
+#libairie mqtt client
 import paho.mqtt.client as mqtt
 
+#addresse ip broker mqtt
 broker_address = "192.168.143.136"
+
+#nom de notre client mqtt
 client = mqtt.Client("camera")
-# Load the cascade
+
+#lecture du fichier contenant les vecteurs pour detecter les visages
 face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
 
-# http://192.168.1.114:99/videostream.cgi?user=admin&pwd=&resolution=32&rate=0
+#http://192.168.1.114:99/videostream.cgi?user=admin&pwd=&resolution=32&rate=0
 #cap = cv2.VideoCapture("http://192.168.143.141:8081/")
+
+#lecture du flux video de la camera connecter en USB
 cap = cv2.VideoCapture(0)
-# To use a video file as input
-# cap = cv2.VideoCapture('filename.mp4')
 print("lancement du flux vidéo")
+
 while True:
     _, img = cap.read()
-    # Convert to grayscale
+    #copnvertie l'image en gris 
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    # Detect the faces
+    #detecte les visages 
     faces = face_cascade.detectMultiScale(gray, 1.1, 4)
-    # Draw the rectangle around each face
+    #dessine un rectancgle autour du visages
     for (x, y, w, h) in faces:
         cv2.rectangle(img, (x, y), (x+w, y+h), (255, 0, 0), 2)
-    # Display
+    #viriables qui enregistre le nombres de rectangles qu'il detecte
     nb_pers = len(faces)
     #print(nb_pers)
-    client.connect(broker_address) #connect to broker
+    
+    #connection au broker
+    client.connect(broker_address)
+    #publie sur le topic le nombre de personne
     client.publish("/batiment/etages/salles/nombre_pers",nb_pers)
     sleep(1)
     #cv2.imshow('img', img)
-    # Stop if escape key is pressed
+    #stop le programme si la touche escape est appuyé
     k = cv2.waitKey(30) & 0xff
     if k == 27:
         break
-# Release the VideoCapture object
 cap.release()
